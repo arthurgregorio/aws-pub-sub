c580c919a2aed75b193cdf59c946713ca4afc9b7
Arthur Gregorio <arthurshakal@gmail.com>
2023-09-19 21:38:41 -0300

    package adjustments

--- sns-pub/src/main/java/br/eti/arthurgregorio/snspub/domain/producers/OrderCreatedNotificationProducer.java
+++ sns-pub/src/main/java/br/eti/arthurgregorio/snspub/domain/producers/OrderCreatedNotificationProducer.java
@@ -1,22 +1,23 @@
-package br.eti.arthurgregorio.snspub.domain.services;
+package br.eti.arthurgregorio.snspub.domain.producers;
 
+import br.eti.arthurgregorio.snspub.domain.services.CreateOrderService;
 import io.awspring.cloud.sns.core.SnsNotification;
 import io.awspring.cloud.sns.core.SnsTemplate;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 @Slf4j
-@Service
-public class OrderCreatedNotificationPublisher {
+@Component
+public class OrderCreatedNotificationProducer {
 
     private final String topic;
 
     private final SnsTemplate snsTemplate;
 
-    public OrderCreatedNotificationPublisher(
+    public OrderCreatedNotificationProducer(
             @Value("${sns.order.notification-topic}") String topic, SnsTemplate snsTemplate) {
         this.topic = topic;
         this.snsTemplate = snsTemplate;
